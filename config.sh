7a249bdb2e3a41f101d1184ae5681a686509c290
Kanagawa Yamada (Alt Account) <kanagawa.kuribayashi@gmail.com>
2025-07-17 21:15:14 +0700

    Move to KanagawaAlt

--- config.sh
+++ config.sh
@@ -14,7 +14,7 @@ KERNEL_REPO="https://github.com/LoggingNewMemory/SuiKernel-android12-5.10"
 KERNEL_BRANCH="suikernel-main"
 KERNEL_DEFCONFIG="gki_defconfig"
 # Release repository
-GKI_RELEASES_REPO="https://github.com/LoggingNewMemory/SuiKernel-Release"
+GKI_RELEASES_REPO="https://github.com/KanagawaAltACC/SuiKernel-GKI-Bulder"
 # Clang
 CLANG_URL="https://github.com/mikeNG/android_prebuilts_clang_kernel_linux-x86_clang-r416183b"
 CLANG_BRANCH="lineage-20.0"
